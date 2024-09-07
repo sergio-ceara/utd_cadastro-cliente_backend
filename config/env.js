9a71357943bb8c3ebcc7d543027d987624efe630
Sérgio Sousa <sergio_ceara@yahoo.com.br>
2024-09-07 20:30:13 -0300

    Ajuste do banco de dados remoto 1

--- config/env.js
+++ config/env.js
@@ -1,9 +1,9 @@
 const env = {
-	database: 'backend',
-    	username: 'root',
-    	password: '',
-    	host: 'localhost',
-    	dialect: 'mysql',
+	database: 'railway',
+    username: 'root',
+    password: '',
+    host: 'localhost',
+    dialect: 'mysql',
 };
   
 module.exports = env;

410f25a7bc1ccc426d52a150fe2622ed0258e43f
Sérgio Sousa <sergio_ceara@yahoo.com.br>
2024-09-07 21:37:56 -0300

    Ajuste do env.js 2

--- config/env.js
+++ config/env.js
@@ -1,8 +1,9 @@
 const env = {
 	database: 'railway',
     username: 'root',
-    password: '',
-    host: 'localhost',
+    password: 'FGdmoFuYchFwZoUAbwLwtMOZkUXEGpbv',
+    host: 'junction.proxy.rlwy.net',
+    port: 55780,
     dialect: 'mysql',
 };
   
